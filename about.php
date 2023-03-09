aea685882f1ba444d6499f8ae2a5a006e3c4a85d
Zacchaeus Aladeokin <zaladeokin@gmail.com>
2023-03-09 15:07:02 +0100

    Service.php and about.php template created

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+require_once('config/autoload.php');
+?>
+
+
+<?php
+//View
+include_once('include/header.php');
+?>
+  <section>
+  <h1>Lumienux Solar</h1>
+  <p>Lumienux solar is a registered enterprise that deals with installation and sales of related product. WE are....</p>
+  </section>
+
+  <?php
+  include_once('include/footer.php');
+  ?>
---
+++ service.php
@@ -0,0 +1,24 @@
+<?php
+require_once('config/autoload.php');
+?>
+
+
+<?php
+//View
+include_once('include/header.php');
+?>
+  <section>
+  <h1>Services</h1>
+  <p>The following services are offered by our engineers:</p>
+  <ul>
+    <li>Solar  & Inverter installation</li>
+    <li>Maintenenance</li>
+    <li>Troubleshooting</li>
+  </ul>
+  <a href="https://api.whatsapp.com/send?phone=2347034205664"><button>Hire an Engineer now</button></a><br><br>
+    
+  </section>
+
+  <?php
+  include_once('include/footer.php');
+  ?>
